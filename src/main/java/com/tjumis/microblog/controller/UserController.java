076c5fcd57dc852b2d16f869a1263add59ebef51
Yong <lomanyong@gmail.com>
2015-01-04 17:46:20 +0800

    update usercontroller with response code

--- src/main/java/com/tjumis/microblog/controller/UserController.java
+++ src/main/java/com/tjumis/microblog/controller/UserController.java
@@ -6,6 +6,8 @@ import com.tjumis.microblog.model.User;
 import com.tjumis.microblog.model.VUser;
 import com.tjumis.microblog.utils.SecurityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -41,17 +43,23 @@ public class UserController {
     public Object login(@RequestParam(value = "username") String username, @RequestParam(value = "password") String password) {
         List<User> users = mUserDao.findUserByUsername(username);
         if (users.size() == 0) {
-            return new ResultResponse(ResultResponse.STATUS_FAILED, "用户名错误");
+            return new ResponseEntity<Object>(
+                    new ResultResponse(ResultResponse.STATUS_FAILED, "用户名错误"),
+                    HttpStatus.FORBIDDEN);
         } else if (users.size() > 1) {
-            return new ResultResponse(ResultResponse.STATUS_FAILED, "账号出错");
+            return new ResponseEntity<Object>(
+                    new ResultResponse(ResultResponse.STATUS_FAILED, "账号出错"),
+                    HttpStatus.FORBIDDEN);
         }
         User user = users.get(0);
         if (user.checkPassword(password)) {
             user.setToken(generateToken(user.getUsername()));
             mUserDao.updateToken(user);
-            return new VUser(user);
+            return new ResponseEntity<Object>(new VUser(user), HttpStatus.OK);
         } else {
-            return new ResultResponse(ResultResponse.STATUS_FAILED, "密码错误");
+            return new ResponseEntity<Object>(
+                    new ResultResponse(ResultResponse.STATUS_FAILED, "密码错误"),
+                    HttpStatus.FORBIDDEN);
         }
     }
 
